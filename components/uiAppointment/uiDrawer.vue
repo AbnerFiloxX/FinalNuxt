bbfc7de1718160a7e30c6261d924bfed4d66757f
Hector <quimat2@hotmail.com>
2023-12-06 08:45:28 -0600

    Ahora el drawer distingue el doctor que inicio sesion (estetico por ahora)

--- components/uiAppointment/uiDrawer.vue
+++ components/uiAppointment/uiDrawer.vue
@@ -1,63 +1,93 @@
 <template>
-    <v-navigation-drawer
-        app
-        color="white"
-        permanent
-        left
-        width="400"
-    >
-        <v-list>
-            <v-list-item>
-                <v-list-item-content class="d-flex justify-center align-center flex-column">
-                    <v-img :src="require('/assets/images/logo2.png')" style="max-width: 80%; object-fit: cover;"></v-img>
-                <v-list-item-avatar size="160">
-                    <v-img :src="require('/assets/images/Dra.jpg')" ></v-img>
-                </v-list-item-avatar>
-                <v-list-item-title class="text-center display-1">
-                    Dr. Anushka Singh
-                </v-list-item-title>
-            </v-list-item-content>
-            </v-list-item>
-        </v-list>
-        <v-divider />
-        
-        <v-list nav dense>
-            <v-list-item
-                v-for="(item, i) in items"
-                :key="i"
-                :to="item.route"
-                link
-            >
-            <v-list-item-icon>
-                    <v-icon>
-                        {{ item.icon }}
-                    </v-icon>
-                </v-list-item-icon>
-                <v-list-item-content>
-                    <v-list-item-title>
-                        {{ item.text }}
-                    </v-list-item-title>
-                </v-list-item-content>
-            </v-list-item>
-        </v-list>
-    </v-navigation-drawer>
+  <v-navigation-drawer app color="white" permanent left width="400">
+    <v-list>
+      <v-list-item>
+        <v-list-item-content
+          class="d-flex justify-center align-center flex-column"
+        >
+          <v-img
+            :src="require('/assets/images/logo2.png')"
+            style="max-width: 80%; object-fit: cover"
+          ></v-img>
+          <v-list-item-avatar size="160">
+            <v-img :src="require('/assets/images/Dra.jpg')"></v-img>
+          </v-list-item-avatar>
+          <v-list-item-title class="text-center display-1">
+            {{ doctorName }}
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+    <v-divider />
+    <v-list nav dense>
+      <v-list-item v-for="(item, i) in items" :key="i" :to="item.route" link>
+        <v-list-item-icon>
+          <v-icon>
+            {{ item.icon }}
+          </v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>
+            {{ item.text }}
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
-
-<script>
+  
+  <script>
 export default {
-    data () {
-        return {
-            items: [
-                {  text: 'Dashboard', icon: 'mdi mdi-view-dashboard', route: '/dashboard'},
-                {  text: 'Appointment', icon: 'mdi mdi-calendar-clock', route: '/appointment'},
-                {  text: 'Patients', icon: 'mdi mdi-account-group', route: '/patients'},
-                {  text: 'Schedule', icon: 'mdi mdi-clock-check-outline', route: '/schedule'}
-            ]
+  data() {
+    return {
+      items: [
+        {
+          text: "Dashboard",
+          icon: "mdi mdi-view-dashboard",
+          route: "/dashboard",
+        },
+        {
+          text: "Appointment",
+          icon: "mdi mdi-calendar-clock",
+          route: "/appointment",
+        },
+        { text: "Patients", icon: "mdi mdi-account-group", route: "/patients" },
+        {
+          text: "Schedule",
+          icon: "mdi mdi-clock-check-outline",
+          route: "/schedule",
+        },
+      ],
+      doctorName: "",
+    };
+  },
+  methods: {
+    async obtenerNombreDoctor() {
+      try {
+        const token = localStorage.getItem("token"); // Obtener el token almacenado
+
+        const response = await fetch("http://localhost:5000/nombre-doctor", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`, // Enviar el token en el encabezado de autorización
+          },
+        });
+
+        const data = await response.json();
+
+        if (data.alert === "success") {
+          this.doctorName = data.nombre;
+        } else {
+          console.error("Error al obtener el nombre del doctor");
         }
-    }
-}
+      } catch (error) {
+        console.error("Error en la solicitud al backend", error);
+      }
+    },
+  },
+  mounted() {
+    this.obtenerNombreDoctor();
+  },
+};
 </script>
-
-<style scoped>
-
-</style>
--- components/uiUsers/uilogin.vue
+++ components/uiUsers/uilogin.vue
@@ -1,93 +1,91 @@
 <template>
-    <v-card elevation="15" color="blue lighten-5" width="600">
-      <v-card-title class="text-h3">
-        Welcome to my App
-      </v-card-title>
-      <v-card-text>
-        <v-form v-model="isValid" ref="formLogin">
-          <v-row>
-            <v-text-field
-              v-model="usuario"
-              placeholder="Escribe tu usuario"
-              label="Usuario"
-              filled
-              :rules="[regla.vacio]"
-            />
-          </v-row>
-          <v-row>
-            <v-text-field
-              v-model="password"
-              type="password"
-              placeholder="Escribe tu contraseña"
-              label="Contraseña"
-              filled
-              :rules="[regla.vacio, regla.cantidad]"
-            />
-          </v-row>
-        </v-form>
-      </v-card-text>
-      <v-card-actions>
-        <v-row class="pa-2 ma-2" justify="space-around" align="center">
-          <v-btn color="green" @click="ingresarSistema">
-            Ingresar
-          </v-btn>
-          <v-btn color="purple" @click="irRegistro">
-            Registro
-          </v-btn>
+  <v-card elevation="15" color="blue lighten-5" width="600">
+    <v-card-title class="text-h3"> Welcome to my App </v-card-title>
+    <v-card-text>
+      <v-form v-model="isValid" ref="formLogin">
+        <v-row>
+          <v-text-field
+            v-model="usuario"
+            placeholder="Escribe tu usuario"
+            label="Usuario"
+            filled
+            :rules="[regla.vacio]"
+          />
         </v-row>
-      </v-card-actions>
-    </v-card>
-  </template>
+        <v-row>
+          <v-text-field
+            v-model="password"
+            type="password"
+            placeholder="Escribe tu contraseña"
+            label="Contraseña"
+            filled
+            :rules="[regla.vacio, regla.cantidad]"
+          />
+        </v-row>
+      </v-form>
+    </v-card-text>
+    <v-card-actions>
+      <v-row class="pa-2 ma-2" justify="space-around" align="center">
+        <v-btn color="green" @click="ingresarSistema"> Ingresar </v-btn>
+        <v-btn color="purple" @click="irRegistro"> Registro </v-btn>
+      </v-row>
+    </v-card-actions>
+  </v-card>
+</template>
   
-  <script>
-  export default {
-    data() {
-      return {
-        usuario: '',
-        password: '',
-        isValid: false,
-        regla: {
-          vacio: (value) => !!value || 'Campo Requerido!!!',
-          cantidad: (value) => value.length > 8 || 'Minimo 9 Caractéres',
-        },
-      };
-    },
-    methods: {
-      async ingresarSistema() {
-        const valid = this.$refs.formLogin.validate();
-  
-        if (valid) {
-          try {
-            const response = await fetch('http://localhost:5000/login', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({
-                usuario: this.usuario,
-                password: this.password,
-              }),
-            });
-  
-            const data = await response.json();
-  
-            if (data.alert === 'Inicio de sesión exitoso') {
-              alert('Inicio de sesión exitoso');
-              this.$router.push(data.ruta); // Redirigir a la ruta del dashboard
-            } else {
-              alert('Credenciales incorrectas');
-            }
-          } catch (error) {
-            alert('Error en la autenticación');
-            console.error(error);
+<script>
+export default {
+  data() {
+    return {
+      usuario: "",
+      password: "",
+      isValid: false,
+      regla: {
+        vacio: (value) => !!value || "Campo Requerido!!!",
+        cantidad: (value) => value.length > 8 || "Mínimo 9 Caracteres",
+      },
+    };
+  },
+  methods: {
+    async ingresarSistema() {
+      const valid = this.$refs.formLogin.validate();
+
+      if (valid) {
+        try {
+          const response = await fetch("http://localhost:5000/login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              usuario: this.usuario,
+              password: this.password,
+            }),
+          });
+
+          const data = await response.json();
+
+          if (data.alert === "Inicio de sesión exitoso") {
+            // Almacenar el token en localStorage
+            localStorage.setItem("token", data.token);
+
+            alert("Inicio de sesión exitoso");
+            // Redirigir a la ruta del dashboard
+            this.$router.push("/dashboard");
+          } else {
+            alert("Credenciales incorrectas");
           }
-        } else {
-          alert('Datos incorrectos');
+        } catch (error) {
+          alert("Error en la autenticación");
+          console.error(error);
         }
-      },
-      irRegistro() {
-        this.$router.push('/registro'); // Esta será la ruta de registro
-      },
+      } else {
+        alert("Datos incorrectos");
+      }
     },
-  };
-  </script>  
+    irRegistro() {
+      this.$router.push("/registro"); // Esta será la ruta de registro
+    },
+  },
+};
+</script>
