082321b3e8dc82e7fbd8c172e88d29e1f46740ab
Hector <quimat2@hotmail.com>
2023-12-06 07:34:27 -0600

    Login y Registro de doctores completamente funcional

--- components/uiUsers/uiRegistro.vue
+++ components/uiUsers/uiRegistro.vue
@@ -1,14 +1,23 @@
 <template>
     <v-card elevation="15" color="blue lighten-5" width="600">
       <v-card-title class="text-h3">
-        Welcome to my App
+        Registro de Usuario
       </v-card-title>
       <v-card-text>
-        <v-form v-model="isValid" ref="formLogin">
+        <v-form v-model="isValid" ref="formRegistro">
+          <v-row>
+            <v-text-field
+              v-model="nombre"
+              placeholder="Ingresa tu nombre"
+              label="Nombre"
+              filled
+              :rules="[regla.vacio]"
+            />
+          </v-row>
           <v-row>
             <v-text-field
               v-model="usuario"
-              placeholder="Escribe tu usuario"
+              placeholder="Ingresa tu usuario"
               label="Usuario"
               filled
               :rules="[regla.vacio]"
@@ -18,7 +27,7 @@
             <v-text-field
               v-model="password"
               type="password"
-              placeholder="Escribe tu contraseña"
+              placeholder="Ingresa tu contraseña"
               label="Contraseña"
               filled
               :rules="[regla.vacio, regla.cantidad]"
@@ -28,11 +37,11 @@
       </v-card-text>
       <v-card-actions>
         <v-row class="pa-2 ma-2" justify="space-around" align="center">
-          <v-btn color="green" @click="ingresarSistema">
-            Ingresar
+          <v-btn color="red" @click="cancelar">
+            Cancelar
           </v-btn>
-          <v-btn color="purple" @click="irRegistro">
-            Registro
+          <v-btn color="green" @click="registrarUsuario">
+            Registrarse
           </v-btn>
         </v-row>
       </v-card-actions>
@@ -43,6 +52,7 @@
   export default {
     data() {
       return {
+        nombre: '',
         usuario: '',
         password: '',
         isValid: false,
@@ -53,17 +63,18 @@
       };
     },
     methods: {
-      async ingresarSistema() {
-        const valid = this.$refs.formLogin.validate();
+      async registrarUsuario() {
+        const valid = this.$refs.formRegistro.validate();
   
         if (valid) {
           try {
-            const response = await fetch('http://localhost:5000/login', {
+            const response = await fetch('http://localhost:5000/new-doctor', {
               method: 'POST',
               headers: {
                 'Content-Type': 'application/json',
               },
               body: JSON.stringify({
+                nombre: this.nombre,
                 usuario: this.usuario,
                 password: this.password,
               }),
@@ -71,22 +82,22 @@
   
             const data = await response.json();
   
-            if (data.alert === 'Inicio de sesión exitoso') {
-              alert('Inicio de sesión exitoso');
-              this.$router.push(data.ruta); // Redirigir a la ruta del dashboard
+            if (data.alert === 'Registro exitoso') {
+              alert('Registro exitoso');
+              this.$router.push('/'); // Redirigir al login después del registro exitoso
             } else {
-              alert('Credenciales incorrectas');
+              alert('Error en el registro');
             }
           } catch (error) {
-            alert('Error en la autenticación');
+            alert('Error en el registro');
             console.error(error);
           }
         } else {
           alert('Datos incorrectos');
         }
       },
-      irRegistro() {
-        this.$router.push('/registro'); // Esta será la ruta de registro
+      cancelar() {
+        this.$router.push('/');
       },
     },
   };
---
+++ pages/registro/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <ui-registro />
+  </template>
+  
+  <script>
+  import uiRegistro from '@/components/uiUsers/uiRegistro.vue'
+  export default {
+    name: 'IndexPage',
+    components: {
+      uiRegistro
+    }
+  }
+  </script>  
